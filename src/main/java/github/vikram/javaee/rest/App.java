585f72ab15a9b0cbc148be85dfc9982545b76372
Vikram Kommaraju <vkommara@cisco.com>
2015-07-23 22:16:52 -0700

    First commit

---
+++ src/main/java/github/vikram/javaee/rest/App.java
@@ -0,0 +1,13 @@
+package github.vikram.javaee.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
